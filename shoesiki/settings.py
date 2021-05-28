476fa631920bf8d89e235175e53a118fecddd04b
fbyaguar <artem170391@ukr.net>
2021-05-28 21:02:39 +0300

    перед изменением карусели

--- shoesiki/settings.py
+++ shoesiki/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
 from pathlib import Path
+import psycopg2
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -77,8 +78,12 @@ WSGI_APPLICATION = 'shoesiki.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'shoes_db',
+        'USER': 'postgres',
+        'PASSWORD': '170391',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
