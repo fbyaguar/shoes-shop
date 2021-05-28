345839bf0636454817facf8f1df710c3164d0fbd
fbyaguar <artem170391@ukr.net>
2021-05-29 00:02:54 +0300

    работа над index.html подключение postgres

--- shoes/views.py
+++ shoes/views.py
@@ -9,6 +9,12 @@ class Home(ListView):
     model = Shoes
     context_object_name = 'shoes'
 
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data()
+        context['first'] = Shoes.objects.first()
+
+        return context
+
     # def get_queryset(self, *args, **kwargs):
     #     return Shoes.objects.filter(tags__name=self.kwargs['slug'])
 
@@ -20,3 +26,5 @@ class ShoesDetailview(DetailView):
     def get_queryset(self, *args, **kwargs):
         return Shoes.objects.get(id=self.kwargs['pk'])
 # Create your views here.
+
+
